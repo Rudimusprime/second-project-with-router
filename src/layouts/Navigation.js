c5cf7a995223fcd6ba7067171cbb2e6b432ea793
DanielB <d.banasiewicz@pdo.com.pl>
2021-11-12 20:27:03 +0100

    files created; nav component created; nav component styled; install and import react router;

---
+++ src/layouts/Navigation.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import '../styles/Navigation.css';
+
+const list = [
+    {
+        name: "start",
+        path: "/",
+        exact: true
+    },
+    {
+        name: "Produkty",
+        path: "/products",
+        exact: true
+    },
+    {
+        name: "Kontakt",
+        path: "/contact",
+        exact: true
+    },
+    {
+        name: "Panel Admina",
+        path: "/admin",
+        exact: true
+    },
+]
+
+const Navigation = () => {
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path}>{item.name}</NavLink>
+        </li>
+    ))
+    return (
+        <nav className="main">
+            <ul>
+                {menu}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
