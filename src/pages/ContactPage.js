29dfeda76f8f20948896339799515dc7b2ca6808
DanielB <d.banasiewicz@pdo.com.pl>
2021-11-13 20:57:38 +0100

    ContactPage is done; ContactPage styles created;

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import '../styles/ContactPage.css'
 
-const ContactPage = () => {
-    return (
-        <h1>Strona kontaktowa</h1>
-    );
+class ContactPage extends React.Component {
+
+    state = {
+        value: "",
+    }
+
+    handleChange = (e) => {
+        if (e.target.value.length > 0) {
+            this.setState({
+                value: e.target.value,
+            })
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            value: "",
+        })
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className="contact">
+                    <form onSubmit={this.handleSubmit}>
+                        <h3>Napisz do nas!</h3>
+                        <textarea placeholder="Wpisz wiadomość..."
+                                  value={this.state.value}
+                                  onChange={this.handleChange}/>
+                        <button>Wyślij</button>
+                    </form>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContactPage;
